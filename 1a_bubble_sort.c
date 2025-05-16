09279eb16ab9c2c84ed671ac0245cb7908e51100
Abhishekkjcodes <155182094+Abhishekkjcodes@users.noreply.github.com>
2025-05-16 10:10:14 +0530

    Update 1a_bubble_sort.c

--- 1a_bubble_sort.c
+++ 1a_bubble_sort.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
         }
     }
 }
 
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr[] = {5, 6, 1, 3};
     int n = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, n);
-    for (int i = 0; i < n; i++) {
+
+    printf("Unsorted Array: ");
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
-    }
+    printf("\n");
+
+    bubbleSort(arr, n);
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
     return 0;
 }
